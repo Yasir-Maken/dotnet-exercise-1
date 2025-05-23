7cbf2a86539d99f5977a3bf365b5c091835285ff
Yasir-Maken <yasir.maken.cs24@gmail.com>
2025-05-24 02:22:23 +0300

    First Exercise: C# & .NET First Program Push

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// Program takes name as input from user and print Hello! the user input name
+public class InputOutput
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine(); // Reads what the user types
+
+        Console.WriteLine($"Hello, {name}!"); // Displays a greeting with the input
+    }
+}
